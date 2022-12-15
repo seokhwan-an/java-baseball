10f74a6d288000aff2c5b6e51d8313db4acb1111
seokhwan-an <asok369258@naver.com>
2022-12-15 17:26:32 +0900

    feat : 사용자의 정답 입력이 숫자 1과 9사이의 수로 구성되어 있는지 검증하는 기능 추가

--- src/main/java/baseball/validation/Validation.java
+++ src/main/java/baseball/validation/Validation.java
@@ -7,12 +7,20 @@ public class Validation {
     // 사용자의 정답 입력을 검증하는 메소드
     public static void validateUserAnswerInput(String input) {
         validateUserAnswerLenght3(input);
+        validateUserAnswerConsistOf1To9(input);
     }
 
-    // 사용자의 정답 입력의 길이가 3인지 검증하는 메소드
+    // 사용자의 정답 입력의 길이가 3인지 검증하는 기능
     private static void validateUserAnswerLenght3(String input) {
         if (input.length() != LENGTH) {
             throw new IllegalArgumentException(USER_ANSWER_LENGTH_ERROR);
+        }
+    }
+
+    // 사용자의 정답 입력값이 1과 9사이의 수로 구성되어있는지 검증하는 기능
+    private static void validateUserAnswerConsistOf1To9(String input) {
+        if (!input.matches(USER_ANSWER_REGEX)) {
+            throw new IllegalArgumentException(USER_ANSWER_CONSIST_OF_1_TO_9_ERROR);
         }
     }
 }
