603aac088bb0624e7e3c769afc4f96c33037bbd8
seokhwan-an <asok369258@naver.com>
2022-12-15 15:54:48 +0900

    feat : 정답이 숫자 1과 9사이의 수로 구성되어있는지 검증하는 기능 추가

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,5 +1,7 @@
 package baseball.domain;
 
+import static baseball.domain.util.Constant.*;
+
 import java.util.List;
 
 public class Computer {
@@ -8,6 +10,10 @@ public class Computer {
 
     // 정답의 길이가 3인지 체크하는 기능
     private boolean checkLength3() {
-        return answerNumber.size() == 3;
+        return answerNumber.size() == LENGTH;
+    }
+
+    private boolean checkNumberConsistOf1To9(int num) {
+        return MIN_NUMBER <= num && num <= MAX_NUMBER;
     }
 }
--- src/main/java/baseball/domain/util/Constant.java
+++ src/main/java/baseball/domain/util/Constant.java
@@ -3,4 +3,6 @@ package baseball.domain.util;
 public class Constant {
 
     public static final int LENGTH = 3;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
 }
