5ff0c9b470058460e62b503ebd74aeb3ead86f20
seokhwan-an <asok369258@naver.com>
2022-12-15 22:03:20 +0900

    feat : 숫자야구 게임 개발완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.Controller.BaseballGameController;
+import baseball.domain.Computer;
+import baseball.domain.Hint;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Computer computer = new Computer();
+        Hint hint = new Hint();
+        BaseballGameController baseballGameController = new BaseballGameController(computer, hint);
+        baseballGameController.run();
     }
 }
