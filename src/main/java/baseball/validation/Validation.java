9b7645ec0171eefbb3b8931ed4486a400e0ce6ce
seokhwan-an <asok369258@naver.com>
2022-12-15 17:39:59 +0900

    feat : 사용자의 정답 입력이 서로다른 수로 구성되었는지 검증하는 기능 추가

--- src/main/java/baseball/validation/Validation.java
+++ src/main/java/baseball/validation/Validation.java
@@ -2,15 +2,22 @@ package baseball.validation;
 
 import static baseball.util.Constant.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Validation {
 
     // 사용자의 정답 입력을 검증하는 메소드
     public static void validateUserAnswerInput(String input) {
         validateUserAnswerLenght3(input);
         validateUserAnswerConsistOf1To9(input);
+        validateUserAnswerOverlap(input);
     }
 
-    // 사용자의 정답 입력의 길이가 3인지 검증하는 기능
+    // 사용자의 정답 입력의 길이가 3인지 검증하는 기
     private static void validateUserAnswerLenght3(String input) {
         if (input.length() != LENGTH) {
             throw new IllegalArgumentException(USER_ANSWER_LENGTH_ERROR);
@@ -21,6 +28,14 @@ public class Validation {
     private static void validateUserAnswerConsistOf1To9(String input) {
         if (!input.matches(USER_ANSWER_REGEX)) {
             throw new IllegalArgumentException(USER_ANSWER_CONSIST_OF_1_TO_9_ERROR);
+        }
+    }
+
+    private static void validateUserAnswerOverlap(String input) {
+        List<String> answer = new ArrayList<>(Arrays.asList(input.split("")));
+        Set<String> check = new HashSet<>(answer);
+        if (check.size() != LENGTH) {
+            throw new IllegalArgumentException(USER_ANSWER_OVERLAP_ERROR);
         }
     }
 }
