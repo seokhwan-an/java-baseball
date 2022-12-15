ee05a020341a017e93bea82eef1ad0c22e8d0c52
seokhwan-an <asok369258@naver.com>
2022-12-15 17:46:18 +0900

    feat : 사용자 정답 입력에 검증 기능 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,5 +1,6 @@
 package baseball.view;
 
+import baseball.validation.Validation;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
@@ -7,6 +8,7 @@ public class InputView {
     // 사용자가 응답하는 기능
     public static String userAnswer() {
         String input = Console.readLine();
+        Validation.validateUserAnswerInput(input);
         return input;
     }
 }
