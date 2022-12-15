ebcc4177455f130f36ee31b465e202ceb38a0025
seokhwan-an <asok369258@naver.com>
2022-12-15 17:12:23 +0900

    feat : 사용자가 정답을 입력하는 기능 추가

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,12 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    // 사용자가 응답하는 기능
+    public static String userAnswer() {
+        String input = Console.readLine();
+        return input;
+    }
+}
