9afd26d307e0cfb046e7bf784f4bcbcebcedf1f8
seokhwan-an <asok369258@naver.com>
2022-12-15 16:04:27 +0900

    feat : 컴퓨터가 정답을 생성하는 기능 추가

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -2,11 +2,28 @@ package baseball.domain;
 
 import static baseball.util.Constant.*;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
 
     private List<Integer> answerNumber;
+
+    public Computer() {
+        answerNumber = new ArrayList<>();
+    }
+
+    public List<Integer> createAnswer() {
+        while(!checkLength3()) {
+            int num = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+            if (checkNumberConsistOf1To9(num) && checkOverlap(num)) {
+                continue;
+            }
+            answerNumber.add(num);
+        }
+        return answerNumber;
+    }
 
     // 정답의 길이가 3인지 체크하는 기능
     private boolean checkLength3() {
