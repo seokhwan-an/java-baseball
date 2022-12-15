e6738603d3fe5d4613c4bcfcb595ce3364f60be7
seokhwan-an <asok369258@naver.com>
2022-12-15 18:25:27 +0900

    test : 힌트를 제공하는 기능 테스트

---
+++ src/test/java/baseball/domain/HintTest.java
@@ -0,0 +1,35 @@
+package baseball.domain;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class HintTest {
+
+    private Hint hint = new Hint();
+
+    @DisplayName("컴퓨터가 제시한 정답과 사용자의 입력값을 비교해서 힌트를 제공하는 기능 테스트")
+    @ParameterizedTest
+    @MethodSource("generateData")
+    void testgiveHint(List<Integer> answer, String userInput, int[] result) {
+        // given
+        int[] hintResult = hint.giveHint(answer, userInput);
+
+        assertThat(hintResult).isEqualTo(result);
+    }
+
+
+    static Stream<Arguments> generateData() {
+        return Stream.of(
+            Arguments.of(Arrays.asList(1,2,3), "321", new int[] {1,2}),
+            Arguments.of(Arrays.asList(3,1,2), "123", new int[] {0,3})
+        );
+    }
+}
