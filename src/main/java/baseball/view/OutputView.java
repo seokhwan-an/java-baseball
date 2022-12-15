b9fad93f3ef3dfa51c6799f75fe9dfb1be0bb7ea
seokhwan-an <asok369258@naver.com>
2022-12-15 19:14:05 +0900

    feat : 힌트를 출력하는 기능 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -7,4 +7,17 @@ public class OutputView {
     public static void printStart() {
         System.out.println(GAME_START);
     }
+
+    public static void printHint(int[] hint) {
+        if(hint[0] == 0 && hint[1] == 0) {
+            System.out.println("낫싱");
+        } else if(hint[0] == 0) {
+            System.out.println(hint[1]+"볼");
+        } else if(hint[1] == 0) {
+            System.out.println(hint[0]+"스트라이크");
+        } else if(hint[0] != 0 && hint[1] != 0) {
+            System.out.println(hint[1]+"볼" + hint[0]+"스트라이크");
+        }
+    }
+
 }
